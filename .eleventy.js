266b2be0351660b71be4954c6a9ff980dc4254b6
William Svoboda <wsvoboda@princeton.edu>
2021-03-22 02:06:14 -0400

    Fix (actually) syntax highlighting, highlight active links, assignment dates

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,41 @@
+const { DateTime } = require("luxon");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
     // Register plugins
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
     eleventyConfig.addPlugin(syntaxHighlight);
+
+    // Custom filters
+    eleventyConfig.addFilter("readableDate", dateObj => {
+        return DateTime.fromJSDate(dateObj).toFormat("ccc, MMM d 'at' t ZZZZ");
+    });
+    eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+        return DateTime.fromJSDate(dateObj);
+    });
+
+    // "HH 'hours and' mm 'minutes'"
 
     // Pass through static files to output
     eleventyConfig.addPassthroughCopy("static");
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("js");
-    
+
+    // Customize Markdown library and settings:
+    let markdownLibrary = markdownIt({
+        html: true,
+        breaks: true,
+        linkify: true
+    }).use(markdownItAnchor, {
+        permalink: true,
+        permalinkClass: "direct-link",
+        permalinkSymbol: "#"
+    });
+    eleventyConfig.setLibrary("md", markdownLibrary);
+
     return {
         // These are all optional, defaults are shown:
         dir: {
